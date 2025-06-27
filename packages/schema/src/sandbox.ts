d660642239936892e198ac84dfe3d9a96a63bd12
oott123 <git@public.oott123.com>
2025-06-27 21:50:41 +0800

    feat: list and create sandbox

--- packages/schema/src/sandbox.ts
+++ packages/schema/src/sandbox.ts
@@ -1,19 +1,26 @@
 import { z } from 'zod'
+import { attachTitle } from './utils.js'
 
 export const createSandboxSchema = z.object({
-  name: z.string().default('sandbox').describe('The name of the sandbox.'),
-  maxLifeSeconds: z
-    .number()
-    .min(1)
-    .max(60 * 60 * 24)
-    .default(60 * 60)
-    .describe('The maximum life time of the sandbox in seconds. Default is 1 hour, max is 1 day.'),
-  projectId: z.string().optional().describe('The project id to use for the sandbox. Use default if not provided.'),
-  specId: z.string().optional().describe('The spec of the sandbox. Use default if not provided.'),
-  datacenterId: z
-    .string()
-    .optional()
-    .describe('The datacenter id to use for the sandbox. Use default if not provided.'),
+  name: attachTitle(z.string().optional().default('sandbox').describe('The name of the sandbox.'), 'Sandbox Name'),
+  maxLifeSeconds: attachTitle(
+    z
+      .number()
+      .min(1)
+      .max(60 * 60 * 24)
+      .default(60 * 60)
+      .describe('The maximum life time of the sandbox in seconds. Default is 1 hour, max is 1 day.'),
+    'Max Life Time',
+  ),
+  projectId: attachTitle(
+    z.string().optional().describe('The project id to use for the sandbox. Use default if not provided.'),
+    'Project',
+  ),
+  specId: attachTitle(z.string().optional().describe('The spec of the sandbox. Use default if not provided.'), 'Spec'),
+  datacenterId: attachTitle(
+    z.string().optional().describe('The datacenter id to use for the sandbox. Use default if not provided.'),
+    'Datacenter',
+  ),
 })
 
 export type CreateSandbox = z.infer<typeof createSandboxSchema>
