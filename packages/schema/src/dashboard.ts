7fbcfb4340bfdfed4da7eb87bc87602a6b903118
oott123 <git@public.oott123.com>
2025-07-20 17:36:37 +0800

    feat: wait list

--- packages/schema/src/dashboard.ts
+++ packages/schema/src/dashboard.ts
@@ -7,3 +7,18 @@ export const statsSchema = z.object({
 })
 
 export type Stats = z.infer<typeof statsSchema>
+
+export const siteConfigSchema = z.object({
+  apiUrl: z.string().describe('API URL'),
+  turnstileSiteKey: z.string().describe('Turnstile site key'),
+})
+
+export type SiteConfig = z.infer<typeof siteConfigSchema>
+
+export const waitListInputSchema = z.object({
+  email: z.string().email().describe('E-Mail'),
+  name: z.string().optional().describe('Name'),
+  useCase: z.string().optional().describe('Use Case'),
+})
+
+export type WaitListInput = z.infer<typeof waitListInputSchema>
