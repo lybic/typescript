17b2715dc5930f28e9e9bc9f185856a7fefc8e56
oott123 <git@public.oott123.com>
2025-07-31 16:03:01 +0800

    feat: lybic playground init

---
+++ packages/playground/src/routes/__root.tsx
@@ -0,0 +1,45 @@
+/// <reference types="vite/client" />
+import type { ReactNode } from 'react'
+import { Outlet, createRootRoute, HeadContent, Scripts } from '@tanstack/react-router'
+import icon from '../assets/icon.svg'
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'Lybic Playground',
+      },
+    ],
+    links: [{ rel: 'icon', href: icon }],
+  }),
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  )
+}
