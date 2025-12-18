d5052e180dff348987a197e9bffe4033be17dc32
翁晓东 <wengxiaodong@tingyutech.com>
2025-12-18 07:39:34 +0000

    feat(image): automate machine image cleanup and remove feature gating

--- packages/schema/src/feature-flag.ts
+++ packages/schema/src/feature-flag.ts
@@ -1,8 +1,6 @@
-export const FEATURE_FLAG_BRING_YOUR_OWN_SANDBOX = 'bring_your_own_sandbox' as const
-export const FEATURE_FLAG_CUSTOM_SPEC_AND_DATACENTER = 'custom_spec_and_datacenter' as const
-export const FEATURE_FLAG_MACHINE_IMAGE = 'machine_image' as const
+export const FEATURE_FLAG_BRING_YOUR_OWN_SANDBOX = 'bring_your_own_sandbox'
+export const FEATURE_FLAG_CUSTOM_SPEC_AND_DATACENTER = 'custom_spec_and_datacenter'
 
 export type FeatureFlagName =
   | typeof FEATURE_FLAG_BRING_YOUR_OWN_SANDBOX
   | typeof FEATURE_FLAG_CUSTOM_SPEC_AND_DATACENTER
-  | typeof FEATURE_FLAG_MACHINE_IMAGE
